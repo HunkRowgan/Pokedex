6c71855a9de529adf1212baffde71a54e6286dc0
HunkRowgan <141590122+HunkRowgan@users.noreply.github.com>
2023-09-12 01:15:54 +0200

    Added various pokemon
    - added simple array of pokemen
    - included basic data (name, type, height)

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,35 @@
-alert('Hello world');
-
-
 //this is a single line comment
 /*this comment
 spans multiple 
 lines
 */
 
-let favouriteFood = "Pizza";
-document.write(favouriteFood);
 
+let pokemonList = [];
+
+pokemonList = [
+
+    {
+        name: 'Bulbasaur',
+        type: ['Grass', 'Poison'],
+        height: 0.7,
+    },
+   
+    {
+        name: 'Ivysaur',
+        type: ['Grass', 'Poison'],
+        height: 1.0,
+    },
+
+    {
+        name: 'Venusaur',
+        type: ['Grass', 'Poison'],
+        height: 0.7,
+    }
+
+];
+
+// Iterate through the pokemonList and display each Pokemon's name and height
+pokemonList.forEach(function (pokemon) {
+    document.write(`Name: ${pokemon.name}, Type: ${pokemon.type.join(', ')}, Height: ${pokemon.height} meters<br>`);
+  });
