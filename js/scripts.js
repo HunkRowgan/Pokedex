5318ab8aa10e9e337ecf0e9a6fe389c2355f4a9f
HunkRowgan <141590122+HunkRowgan@users.noreply.github.com>
2023-09-20 00:04:47 +0200

    Added conditional to pokemon table
    - added condition for height > 1m

--- js/scripts.js
+++ js/scripts.js
@@ -25,11 +25,31 @@ pokemonList = [
         name: 'Venusaur',
         type: ['Grass', 'Poison'],
         height: 0.7,
+    },
+    {
+        name: 'Butterfree',
+        type: ['Bug', 'Flying'],
+        height: 1.1,
     }
 
 ];
 
 // Iterate through the pokemonList and display each Pokemon's name and height
-pokemonList.forEach(function (pokemon) {
+/* pokemonList.forEach(function (pokemon) {
     document.write(`Name: ${pokemon.name}, Type: ${pokemon.type.join(', ')}, Height: ${pokemon.height} meters<br>`);
-  });
+  })
+*/
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+    
+
+    if (pokemonList[i].height > 1) {
+        document.write(`Name: ${pokemonList[i].name} Type: ${pokemonList[i].type} Height: ${pokemonList[i].height} metres - Wow, that’s big!`)
+    } else {
+        document.write(`Name: ${pokemonList[i].name} Type: ${pokemonList[i].type} Height: ${pokemonList[i].height} metres<br>`)
+    }
+}
+
+;
+  
