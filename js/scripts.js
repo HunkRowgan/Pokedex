cdf1e01c2c79fb5ae223ac41b3c72af3ddf6ea46
HunkRowgan <141590122+HunkRowgan@users.noreply.github.com>
2023-10-06 13:51:32 +0200

    ex 1.8
    - added modal with image and height

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,8 @@ lines
 let pokemonRepository = (function () {
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150'; // variable to include pokemon api ?limit=150 sets number of pokemon to retain from api
-
+  let modalContainer = document.querySelector('#modal-container');
+  let dialogPromiseReject;
 
 function showDetails (pokemon){
     console.log(pokemon);
@@ -75,6 +76,7 @@ function loadDetails(item) {
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
       item.types = details.types;
+      showModal(item);
     }).catch(function (e) {
       console.error(e);
     });
@@ -85,6 +87,57 @@ function loadDetails(item) {
       console.log(pokemon);
     });
   }
+
+  // add new function for showModal
+  function showModal(pokemon) {
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = pokemon.name;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = 'Height: ' + pokemon.height;
+
+    let imageElement = document.createElement('img');
+    imageElement.src = imageElement.imageUrl;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modal.appendChild(imageElement);
+    modalContainer.appendChild(modal);
+
+
+   modalContainer.classList.add('is-visible');
+
+  }
+// add new function hideModal
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+      if (dialogPromiseReject) {
+      dialogPromiseReject();
+      dialogPromiseReject = null;
+    }
+  }
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();  
+    }
+  })
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
 
 return {
     add: add,
